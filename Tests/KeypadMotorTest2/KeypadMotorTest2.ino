a98298673ed5bdc4b6402939f655b4e192d31381
Patrick Miller <thepdm@gmail.com>
2015-03-31 23:57:00 -0700

    reorganized to declutter and modularize

--- Tests/KeypadMotorTest2/KeypadMotorTest2.ino
+++ Tests/KeypadMotorTest2/KeypadMotorTest2.ino
@@ -6,26 +6,22 @@
 // VARIABLE INITIALIZATIONS AND CONSTANTS
 ////////////////////////////////////////////////////////////////////////////////
 
-enum Menu_Items
+enum Main_Menu
 {
   mode_demo,
   mode_10_cycles,
   mode_100_cycles,
-  screen_about,
-  FINAL_MENU_ITEMS_ENTRY
+  mode_about,
+  MAIN_MENU_ENUM_LAST_ITEM
 };
 
-const char* Menu_Items_As_Strings[] =
+const char* Main_Menu_Names[] =
 {
   "Demo mode",
+  "10 Cycles",
   "100 Cycles",
-  "10M Cycles",
   "About"
 };
-
-const char* testOptions[] = {"#1: Demo mode", "#2: 100 Cycles","#3: 10M Cycles","About"};
-int testOptionsCount = 4;
-int testNumber = 0; //default test number
 
 LCDKeypad lcd;
 AccelStepper stepper(AccelStepper::DRIVER, 22, 24);
@@ -64,29 +60,110 @@ byte c_select[8] = {
 };
 
 ////////////////////////////////////////////////////////////////////////////////
+// MAIN
+////////////////////////////////////////////////////////////////////////////////
+
+void setup()
+{
+  stepper.setMaxSpeed(4000); //max supported is about 4000
+  stepper.setAcceleration(10000);
+
+  lcd.createChar(1, c_select);
+  lcd.createChar(2, c_up);
+  lcd.createChar(3, c_down);
+  lcd.begin(16, 2);
+
+  //Start getting user input
+  lcd.clear();
+  lcd.print("Pick Mode");
+  lcd.setCursor(0, 1);
+  lcd.print("to begin.");
+  delay(1000);
+
+  int main_menu_choice = showMenu(Main_Menu_Names, MAIN_MENU_ENUM_LAST_ITEM);
+
+  switch (main_menu_choice)
+  {
+    case mode_demo:
+      doStepper(1);
+      break;
+    case mode_10_cycles:
+      doStepper(10);
+      break;
+    case mode_100_cycles:
+      doStepper(100);
+      break;
+    case mode_about:
+      showAboutScreen();
+      break;
+  }
+
+  if (main_menu_choice == mode_about)
+  {
+
+  }
+
+  //END OF PROGRAM
+  lcd.clear();
+  lcd.print("Done.");
+}
+
+void loop()
+{
+
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
 // FUNCTIONS
 ////////////////////////////////////////////////////////////////////////////////
+
+// LCD KEYPAD LOWER LEVEL
+
+int waitButton()
+{
+  int which_button;
+  waitReleaseButton;
+  lcd.blink();
+  while ((which_button = lcd.button()) == KEYPAD_NONE)
+  {
+  }
+  delay(50);
+  lcd.noBlink();
+  return which_button;
+}
+
+void waitReleaseButton()
+{
+  delay(50);
+  while (lcd.button() != KEYPAD_NONE)
+  {
+  }
+  delay(50);
+}
+
+// USER INTERFACE
 
 void showAboutScreen()
 {
   lcd.print("Knee Wear Tester");
-  delay(500);
+  delay(1000);
   lcd.clear();
   lcd.print("Designed 2015 by");
   lcd.setCursor(0, 1);
   lcd.print(" Chris Blaydon, ");
-  delay(500);
+  delay(1000);
   lcd.clear();
   lcd.print(" Alan Bylund &");
   lcd.setCursor(0, 1);
   lcd.print(" Patrick Miller");
-  delay(500);
+  delay(1000);
 }
 
-int showMenu()
+int showMenu(const char** menu_item_names,int menu_length)
 {
   //RETURNS INT OF SELECTED MENU ITEM
-  int menu_length = FINAL_MENU_ITEMS_ENTRY;
+  //int menu_length = FINAL_MENU_ITEMS_ENTRY;
   int which_button = 0;
   bool has_chosen = 0;
   int menu_active_item = 0; //always start at the first item
@@ -95,7 +172,7 @@ int showMenu()
     lcd.clear();
     lcd.print(menu_active_item+1);
     lcd.print(".");
-    lcd.print(Menu_Items_As_Strings[menu_active_item]);
+    lcd.print(menu_item_names[menu_active_item]);
     lcd.setCursor(0, 1);
     lcd.write(1);
     lcd.print(" Sel ");
@@ -121,7 +198,7 @@ int showMenu()
     {
       menu_active_item++;
       //loop back if maximum
-      if (menu_active_item == testOptionsCount) menu_active_item = 0;
+      if (menu_active_item == menu_length) menu_active_item = 0;
     }
     if (which_button == KEYPAD_UP)
     {
@@ -134,121 +211,28 @@ int showMenu()
   return menu_active_item;
 }
 
-////////////////////////////////////////////////////////////////////////////////
-// MAIN
-////////////////////////////////////////////////////////////////////////////////
+// STEPPER
 
-void setup()
+void doStepperOneCycle()
 {
-  stepper.setMaxSpeed(4000); //max supported is about 4000
-  stepper.setAcceleration(10000);
+  //Moves back and forth once, then returns.
+  stepper.runToNewPosition(1600);
+  stepper.runToNewPosition(0);
+}
 
-  lcd.createChar(1, c_select);
-  lcd.createChar(2, c_up);
-  lcd.createChar(3, c_down);
-  lcd.begin(16, 2);
-
-  //Start getting user input
-  lcd.clear();
-  lcd.print("Pick Test Number");
-  lcd.setCursor(0, 1);
-  lcd.print("to begin.");
-  delay(1000);
-
-  //waitButton();
-  //waitReleaseButton();
-
-  int testOptionsCount = FINAL_MENU_ITEMS_ENTRY;
-
-  bool hasChosen = 0;
-  int which_button = 0;
-  // while (!hasChosen) {
-  //   lcd.clear();
-  //   lcd.print(testOptions[testNumber]);
-  //   lcd.setCursor(0, 1);
-  //   lcd.write(1);
-  //   lcd.print(" Sel ");
-  //   lcd.write(2);
-  //   lcd.print(" Up ");
-  //   lcd.write(3);
-  //   lcd.print(" Dn");
-
-    //Wait for correct input
-    //TODO: Add debouncing support
-
-    showMenu();
-
-  //   do
-  //   {
-  //     which_button = waitButton();
-  //   }
-  //   while (!(which_button == KEYPAD_SELECT || which_button == KEYPAD_UP || which_button == KEYPAD_DOWN));
-  //
-  //   if (which_button == KEYPAD_SELECT)
-  //   {
-  //     hasChosen = true; //ends loop
-  //   }
-  //   if (which_button == KEYPAD_DOWN)
-  //   {
-  //     testNumber++;
-  //     //loop back if maximum
-  //     if (testNumber == testOptionsCount) testNumber = 0;
-  //   }
-  //   if (which_button == KEYPAD_UP)
-  //   {
-  //     testNumber--;
-  //     //loop back if minimum
-  //     if (testNumber == -1) testNumber = testOptionsCount - 1;
-  //   }
-  //   waitReleaseButton();
-  // }
-
-  if (testNumber == 3)
+void doStepper(int repetitions)
+{
+  int starting_time = millis();
+  //Does as many stepper cycles as requested
+  for (int i = 1; i < (repetitions + 1); i++)
   {
-    showAboutScreen();
-  }
-
-  //Just some demo code for now
-  for (int i = 1; i < 10; i++) {
     lcd.clear();
-    lcd.print("Testing...");
+    lcd.print("time=");
+    lcd.print(millis()-starting_time);
+    lcd.print("ms");
     lcd.setCursor(0, 1);
-    // print the number of seconds since reset:
-    lcd.print("Cycle # ");
+    lcd.print("Cycle #");
     lcd.print(i);
-
-    stepper.runToNewPosition(1600);
-    stepper.runToNewPosition(0);
+    doStepperOneCycle();
   }
-  lcd.clear();
-  lcd.print("Done");
-  //delay(1000);
-}
-
-void loop()
-{
-
-
-}
-
-int waitButton()
-{
-  int which_button;
-  waitReleaseButton;
-  lcd.blink();
-  while ((which_button = lcd.button()) == KEYPAD_NONE)
-  {
-  }
-  delay(50);
-  lcd.noBlink();
-  return which_button;
-}
-
-void waitReleaseButton()
-{
-  delay(50);
-  while (lcd.button() != KEYPAD_NONE)
-  {
-  }
-  delay(50);
 }
