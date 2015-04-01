f46f1c3a55d417a0a83bec6d21bb3ed3b1b437c5
Patrick Miller <thepdm@gmail.com>
2015-03-31 23:21:56 -0700

    phasing in new menu chooser

--- Tests/KeypadMotorTest2/KeypadMotorTest2.ino
+++ Tests/KeypadMotorTest2/KeypadMotorTest2.ino
@@ -15,7 +15,7 @@ enum Menu_Items
   FINAL_MENU_ITEMS_ENTRY
 };
 
-const char* Menu_Item_As_Strings[] =
+const char* Menu_Items_As_Strings[] =
 {
   "Demo mode",
   "100 Cycles",
@@ -83,6 +83,57 @@ void showAboutScreen()
   delay(500);
 }
 
+int showMenu()
+{
+  //RETURNS INT OF SELECTED MENU ITEM
+  int menu_length = FINAL_MENU_ITEMS_ENTRY;
+  int which_button = 0;
+  bool has_chosen = 0;
+  int menu_active_item = 0; //always start at the first item
+
+  while (!has_chosen) {
+    lcd.clear();
+    lcd.print(menu_active_item+1);
+    lcd.print(".");
+    lcd.print(Menu_Items_As_Strings[menu_active_item]);
+    lcd.setCursor(0, 1);
+    lcd.write(1);
+    lcd.print(" Sel ");
+    lcd.write(2);
+    lcd.print(" Up ");
+    lcd.write(3);
+    lcd.print(" Dn");
+
+    //Waits for correct input
+    //TODO: Add debouncing support
+
+    do
+    {
+      which_button = waitButton();
+    }
+    while (!(which_button == KEYPAD_SELECT || which_button == KEYPAD_UP || which_button == KEYPAD_DOWN));
+
+    if (which_button == KEYPAD_SELECT)
+    {
+      has_chosen = true; //ends loop
+    }
+    if (which_button == KEYPAD_DOWN)
+    {
+      menu_active_item++;
+      //loop back if maximum
+      if (menu_active_item == testOptionsCount) menu_active_item = 0;
+    }
+    if (which_button == KEYPAD_UP)
+    {
+      menu_active_item--;
+      //loop back if minimum
+      if (menu_active_item == -1) menu_active_item = menu_length - 1;
+    }
+    waitReleaseButton();
+  }
+  return menu_active_item;
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // MAIN
 ////////////////////////////////////////////////////////////////////////////////
@@ -111,44 +162,46 @@ void setup()
 
   bool hasChosen = 0;
   int which_button = 0;
-  while (!hasChosen) {
-    lcd.clear();
-    lcd.print(testOptions[testNumber]);
-    lcd.setCursor(0, 1);
-    lcd.write(1);
-    lcd.print(" Sel ");
-    lcd.write(2);
-    lcd.print(" Up ");
-    lcd.write(3);
-    lcd.print(" Dn");
+  // while (!hasChosen) {
+  //   lcd.clear();
+  //   lcd.print(testOptions[testNumber]);
+  //   lcd.setCursor(0, 1);
+  //   lcd.write(1);
+  //   lcd.print(" Sel ");
+  //   lcd.write(2);
+  //   lcd.print(" Up ");
+  //   lcd.write(3);
+  //   lcd.print(" Dn");
 
     //Wait for correct input
     //TODO: Add debouncing support
 
-    do
-    {
-      which_button = waitButton();
-    }
-    while (!(which_button == KEYPAD_SELECT || which_button == KEYPAD_UP || which_button == KEYPAD_DOWN));
+    showMenu();
 
-    if (which_button == KEYPAD_SELECT)
-    {
-      hasChosen = true; //ends loop
-    }
-    if (which_button == KEYPAD_DOWN)
-    {
-      testNumber++;
-      //loop back if maximum
-      if (testNumber == testOptionsCount) testNumber = 0;
-    }
-    if (which_button == KEYPAD_UP)
-    {
-      testNumber--;
-      //loop back if minimum
-      if (testNumber == -1) testNumber = testOptionsCount - 1;
-    }
-    waitReleaseButton();
-  }
+  //   do
+  //   {
+  //     which_button = waitButton();
+  //   }
+  //   while (!(which_button == KEYPAD_SELECT || which_button == KEYPAD_UP || which_button == KEYPAD_DOWN));
+  //
+  //   if (which_button == KEYPAD_SELECT)
+  //   {
+  //     hasChosen = true; //ends loop
+  //   }
+  //   if (which_button == KEYPAD_DOWN)
+  //   {
+  //     testNumber++;
+  //     //loop back if maximum
+  //     if (testNumber == testOptionsCount) testNumber = 0;
+  //   }
+  //   if (which_button == KEYPAD_UP)
+  //   {
+  //     testNumber--;
+  //     //loop back if minimum
+  //     if (testNumber == -1) testNumber = testOptionsCount - 1;
+  //   }
+  //   waitReleaseButton();
+  // }
 
   if (testNumber == 3)
   {
